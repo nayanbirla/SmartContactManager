d809cef4b5edf58b9fa1c9b09c3bb0f551a4a2d7
nayan <65284466+nayanbirla@users.noreply.github.com>
2023-10-25 22:36:24 +0530

    Add files via upload

---
+++ smartcontactmanage/src/main/java/com/validate/service/EmailService.java
@@ -0,0 +1,34 @@
+package com.validate.service;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+
+	@Service
+	public class EmailService {
+         
+		@Autowired
+		private JavaMailSender mailSender;
+		
+		public boolean sendEmail(String subject,String body,String to) {
+			boolean b=false;
+		   SimpleMailMessage message=new SimpleMailMessage();	
+		   message.setFrom("shoplifyindia@gmail.com");
+		   message.setTo(to);
+		   message.setText(body);
+		   message.setSubject(subject);
+		   
+		   mailSender.send(message);
+		   
+		   System.out.println("Mail Sent Successfully...");
+		   b=true;
+		   return b;
+		   
+		}
+		
+	}
+
